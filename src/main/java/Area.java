50f4aefcccf43123a3ffe613c19dd4341e9c94d3
omarmoustafa11 <omaroj99@yahoo.com>
2022-09-06 15:22:46 -0400

    sout

--- src/main/java/Area.java
+++ src/main/java/Area.java
@@ -2,6 +2,7 @@ public class Area {
     public static void main(String[] args) {
 
         int Grade = (int) (Math.random()*(100));
+        System.out.println(Grade);
         if (Grade >+ 90) {
             System.out.println("you got an A");
         }  else if (Grade >= 80) {
