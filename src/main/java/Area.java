78c0fc26bf64a976bd4199df7d375badcc0e3d47
omarmoustafa11 <omaroj99@yahoo.com>
2022-09-06 15:10:01 -0400

    exam

---
+++ src/main/java/Area.java
@@ -0,0 +1,26 @@
+public class Area {
+    public static void main(String[] args) {
+
+        int Grade = (int) (Math.random()*(100));
+        if (Grade >+ 90) {
+            System.out.println("you got an A");
+        }  else if (Grade >= 80) {
+            System.out.println("u got a B");
+        }
+        else if (Grade >= 70){
+            System.out.println("you got a C");
+        }
+        else if (Grade >= 60){
+        System.out.println("you got a D");
+        }
+         else {
+            System.out.println("you got an F");
+        }
+
+
+
+
+
+        }
+    }
+
---
+++ src/main/java/Square.java
@@ -0,0 +1,24 @@
+public class Square {
+
+
+    public static double calculateArea;
+
+
+    public double length;
+
+    public Square(double length) {
+    }
+
+
+    public double Square(double length) {
+        this.length = length;
+
+        double calculateArea;
+        return Math.pow(this.length, 2 );
+
+        }
+
+
+    }
+
+
